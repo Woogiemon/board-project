9f17ad13a549cf3e3063a9a111f1dc6a2bfa2fda
JIN <taschen94@gmail.com>
2022-11-13 21:12:23 +0900

    test: JPA Auditing

--- src/test/java/com/woogiemon/board/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/woogiemon/board/domain/posts/PostsRepositoryTest.java
@@ -2,9 +2,10 @@ package com.woogiemon.board.domain.posts;
 
 import static org.assertj.core.api.Assertions.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,7 +16,7 @@ class PostsRepositoryTest {
 	@Autowired
 	PostsRepository postsRepository;
 
-	@AfterEach
+	@BeforeEach
 	void cleanAll() {
 		postsRepository.deleteAll();
 	}
@@ -40,5 +41,30 @@ class PostsRepositoryTest {
 		Posts posts = postsList.get(0);
 		assertThat(posts.getTitle()).isEqualTo(title);
 		assertThat(posts.getContent()).isEqualTo(content);
+	}
+
+	@Test
+	void BaseTimeEntity_등록() {
+
+		//given
+		LocalDateTime now =
+			LocalDateTime.of(2022, 10, 1, 0, 0, 0);
+		postsRepository.save(Posts.builder()
+			.title("title")
+			.content("content")
+			.author("author")
+			.build());
+
+		//when
+		List<Posts> postsList = postsRepository.findAll();
+
+		//then
+		Posts posts = postsList.get(0);
+
+		System.out.println(
+			">>>>>>>>> createdDate = " + posts.getCreatedDate() + ", modifiedDate = " + posts.getModifiedDate());
+
+		assertThat(posts.getCreatedDate()).isAfter(now);
+		assertThat(posts.getModifiedDate()).isAfter(now);
 	}
 }
