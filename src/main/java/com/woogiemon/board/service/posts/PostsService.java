c2e2892b42beb3ab7c2defdc3f258634447ce798
JIN <taschen94@gmail.com>
2022-11-13 20:45:25 +0900

    feat: Posts 수정, 조회 API 생성

--- src/main/java/com/woogiemon/board/service/posts/PostsService.java
+++ src/main/java/com/woogiemon/board/service/posts/PostsService.java
@@ -4,8 +4,11 @@ import javax.transaction.Transactional;
 
 import org.springframework.stereotype.Service;
 
+import com.woogiemon.board.domain.posts.Posts;
 import com.woogiemon.board.domain.posts.PostsRepository;
+import com.woogiemon.board.web.dto.PostsResponseDto;
 import com.woogiemon.board.web.dto.PostsSaveRequestDto;
+import com.woogiemon.board.web.dto.PostsUpdateRequestDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,5 +21,22 @@ public class PostsService {
 	@Transactional
 	public Long save(PostsSaveRequestDto requestDto) {
 		return postsRepository.save(requestDto.toEntity()).getId();
+	}
+
+	@Transactional
+	public Long update(Long id, PostsUpdateRequestDto requestDto) {
+		Posts posts = postsRepository.findById(id)
+			.orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+		posts.update(requestDto.getTitle(), requestDto.getContent());
+
+		return id;
+	}
+
+	public PostsResponseDto findById(Long id) {
+		Posts entity = postsRepository.findById(id)
+			.orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+
+		return new PostsResponseDto(entity);
 	}
 }
--- src/main/java/com/woogiemon/board/web/PostsApiController.java
+++ src/main/java/com/woogiemon/board/web/PostsApiController.java
@@ -1,11 +1,16 @@
 package com.woogiemon.board.web;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.woogiemon.board.service.posts.PostsService;
+import com.woogiemon.board.web.dto.PostsResponseDto;
 import com.woogiemon.board.web.dto.PostsSaveRequestDto;
+import com.woogiemon.board.web.dto.PostsUpdateRequestDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -18,5 +23,15 @@ public class PostsApiController {
 	@PostMapping("/posts")
 	public Long save(@RequestBody PostsSaveRequestDto requestDto) {
 		return postsService.save(requestDto);
+	}
+
+	@PutMapping("/posts/{id}")
+	public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+		return postsService.update(id, requestDto);
+	}
+
+	@GetMapping("/posts/{id}")
+	public PostsResponseDto findById(@PathVariable Long id) {
+		return postsService.findById(id);
 	}
 }
